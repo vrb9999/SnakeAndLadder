6b69cca631ff8f57ef743479c085e5cc57ebc6f3
vrb9999 <vbiradar9999@gmail.com>
2022-04-29 00:05:45 +0530

    UC-3 Day-4 Checking for an option(ladder, snake or no play)

---
+++ CheckOption/CheckOption/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CheckOption
+{
+    class Program
+    {
+        public const int ladder = 1;
+        public const int snake = 2;
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            int option = random.Next(0, 3);
+            Console.WriteLine("Option is : " + option);
+            switch (option)
+            {
+                case ladder:
+                    position += dice;
+                    break;
+
+                case snake:
+                    position -= dice;
+                    if (position < 0)
+                        position = 0;
+                    break;
+
+                default: break;
+            }
+            Console.WriteLine("The player rolled a die  " + dice + ",and now at position : " + position);
+        }
+    }
+}
