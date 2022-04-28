fd7a26599aba3f24beb1014f9b89f34466eda963
vrb9999 <vbiradar9999@gmail.com>
2022-04-29 00:07:20 +0530

    UC-4 Day-4 Player reaches winning position 100)

---
+++ WinningPosition/WinningPosition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WinningPosition
+{
+    class Program
+    {
+        public const int ladder = 1;
+        public const int snake = 2;
+        static void Main(string[] args)
+        {
+            int position = 0;
+            while (position < 100)
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case ladder:
+                        Console.WriteLine("Ladder, step up");
+                        position += dice;
+                        break;
+
+                    case snake:
+                        Console.WriteLine("Snake, step down");
+                        position -= dice;
+                        if (position < 0)
+                            position = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("You cannot move");
+                        break;
+                }
+                Console.WriteLine("The player rolled a die " + dice + ",and now at position : " + position);
+            }
+        }
+    }
+}
