c137e059ce6a8f7ea6c5db13b79dfb9228bdce07
vrb9999 <vbiradar9999@gmail.com>
2022-04-29 00:03:30 +0530

    UC-2 Day-4 Rolling a die

---
+++ RollingDie/RollingDie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RollingDie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine("Player Position : " + position + "  Die rolled : " + dice);
+        }
+    }
+}
