b757d735ac99409c27f911b47cccbb0f26ebf474
vrb9999 <vbiradar9999@gmail.com>
2022-04-29 00:08:26 +0530

    UC-5 Day-4 Player reaches exact winning position 100)

---
+++ ExactWinningPosition/ExactWinningPosition/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExactWinningPosition
+{
+    class Program
+    {
+        public const int ladder = 1;
+        public const int snake = 2;
+        static void Main(string[] args)
+        {
+            int position = 0;
+            while (position < 100)
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case ladder:
+                        Console.WriteLine("Ladder, step up");
+                        position += dice;
+                        break;
+
+                    case snake:
+                        Console.WriteLine("Snake, step down");
+                        position -= dice;
+                        if (position < 0)
+                            position = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("You cannot move");
+                        break;
+                }
+                if (position > 100)
+                {
+                    position -= dice;
+                    Console.WriteLine("Cannot move, roll again");
+                }
+                Console.WriteLine("The player rolled a die " + dice + ",and now at position : " + position);
+            }
+        }
+    }
+}
