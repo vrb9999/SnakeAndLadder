f3d79f4f0d6d2945b40712ba042f7cefc95a3831
vrb9999 <vbiradar9999@gmail.com>
2022-04-29 00:01:01 +0530

    UC-1 Day-4 Player starting position

---
+++ PlayerStartPosition/PlayerStartPosition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayerStartPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Player = 0;
+            Console.WriteLine("Player Position is " + Player);
+        }
+    }
+}
