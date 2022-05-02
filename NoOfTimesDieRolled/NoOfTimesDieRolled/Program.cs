763e4f956e168d46eaeac2e73c849049b65ef672
vrb9999 <vbiradar9999@gmail.com>
2022-05-02 09:18:18 +0530

    UC-6 Day-7 Number of times die rolled to win

--- NoOfTimesDieRolled/NoOfTimesDieRolled/Program.cs
+++ NoOfTimesDieRolled/NoOfTimesDieRolled/Program.cs
@@ -4,18 +4,21 @@ namespace NoOfTimesDieRolled
 {
     class Program
     {
+        //constatnts
         public const int ladder = 1;
         public const int snake = 2;
         static void Main(string[] args)
         {
             int position = 0;
             int rollcount = 0;
+            //Loop for player position reaches 100
             while (position < 100)
             {
                 rollcount++;
                 Random random = new Random();
                 int dice = random.Next(1, 7);
                 int option = random.Next(0, 3);
+                //Player next position - Move forward, move backward, no moves
                 switch (option)
                 {
                     case ladder:
@@ -34,6 +37,7 @@ namespace NoOfTimesDieRolled
                         Console.WriteLine("You cannot move");
                         break;
                 }
+                //Not to exceed position 100, player rolls die to get exact position of 100
                 if (position > 100)
                 {
                     position -= dice;
